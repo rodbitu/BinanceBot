f316c97741243eb6100a65306744bf86607b7a28
Rodrigo Bitu de Moraes <66644281+rodbitu@users.noreply.github.com>
2022-04-22 22:41:12 -0300

    First commit

---
+++ api.js
@@ -0,0 +1,75 @@
+const axios = require('axios');
+const queryString = require('qs');
+const crypto = require('crypto');
+
+const apiKey = process.env.API_KEY_TEST;
+const apiSecret = process.env.SECRET_KEY_TEST;
+const apiUrl = process.env.API_URL_TEST;
+
+const privateCall = async (path, data = {}, method = 'GET') => {
+  const timestamp = Date.now();
+  const signature = crypto
+    .createHmac('sha256', apiSecret)
+    .update(`${queryString.stringify({ ...data, timestamp })}`)
+    .digest('hex');
+
+  const newData = { ...data, timestamp, signature };
+  const qs = `?${queryString.stringify(newData)}`;
+
+  try {
+    const result = await axios({
+      method,
+      url: `${apiUrl}${path}${qs}`,
+      headers: { 'X-MBX-APIKEY': apiKey },
+    });
+    return result.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const accountInfo = async () => {
+  return privateCall('/v3/account');
+};
+
+async function newOrder(
+  symbol,
+  quantity,
+  price,
+  side = 'BUY',
+  type = 'MARKET',
+) {
+  const data = { symbol, side, type, quantity };
+
+  if (price) data.price = parseInt(price);
+  if (type === 'LIMIT') data.timeInForce = 'GTC';
+
+  return privateCall('/v3/order', data, 'POST');
+}
+
+const publicCall = async (path, data, method = 'GET') => {
+  try {
+    const qs = data ? `?${queryString.stringify(data)}` : '';
+    const result = await axios({
+      method,
+      url: `${apiUrl}${path}${qs}`,
+    });
+    return result.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const time = async () => {
+  return publicCall('/v3/time');
+};
+
+const depth = async (symbol, limit = 5) => {
+  return publicCall('/v3/depth', { symbol, limit });
+};
+
+const exchangeInfo = async () => {
+  return publicCall('/v3/exchangeInfo');
+};
+
+module.exports = { time, depth, exchangeInfo, accountInfo, newOrder };
---
+++ index.js
@@ -0,0 +1,50 @@
+const api = require('./api');
+const symbol = process.env.SYMBOL;
+
+console.log('Iniciando monitoramento!\n');
+
+setInterval(async () => {
+  const result = await api.depth(symbol);
+  const account = await api.accountInfo();
+  const coins = account.balances.filter((b) => symbol.indexOf(b.asset) !== -1);
+  const sellPrice = parseFloat(result.asks[0][0]);
+  const profitability = parseFloat(process.env.PROFITABILITY);
+  const sellOrder = await api.newOrder(
+    symbol,
+    1,
+    sellPrice * profitability,
+    'SELL',
+    'LIMIT',
+  );
+  const carteiraUSD = parseFloat(
+    coins.find((c) => c.asset.endsWith('USD')).free,
+  );
+
+  console.log('Mercado');
+
+  console.log(
+    result.bids.length ? `Compra: ${result.bids[0][0]}` : 'Sem Compras',
+  );
+  console.log(
+    result.asks.length ? `Venda: ${result.asks[0][0]}` : 'Sem Vendas',
+  );
+
+  console.log('\nCarteira');
+  console.log(coins);
+
+  if (sellPrice < 1000) {
+    console.log('Preço está bom. Verificando se tenho grana...');
+    if (sellPrice <= carteiraUSD) {
+      console.log('Tenho! Comprando!');
+      const buyOrder = await api.newOrder(symbol, 1);
+      console.log(`orderId: ${buyOrder.orderId}`);
+      console.log(`status: ${buyOrder.status}`);
+
+      console.log(`Posicionando venda. Ganho de ${profitability}`);
+      console.log(`orderId: ${sellOrder.orderId}`);
+      console.log(`status: ${sellOrder.status}`);
+    }
+  }
+
+  console.log('\n---------------------------------\n');
+}, process.env.CRAWLER_INTERVAL || 3000);
